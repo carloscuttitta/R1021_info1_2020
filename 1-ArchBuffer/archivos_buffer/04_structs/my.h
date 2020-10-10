4170738b439d8a9eef16a5ecc15f0b406471f828
carloscuttitta <carloscuttitta@hotmail.com>
2020-10-10 20:46:41 -0300

    acomodar cosas

--- 1-ArchBuffer/archivos_buffer/04_structs/my.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-typedef struct {
-	int id;
-	char msg[20];
-	}data_t;
---
+++ enviaArchTCP/server-push.c
@@ -0,0 +1,96 @@
+#include <stdio.h>		// Standard I/O
+#include <stdlib.h> 	        // Libreria standard de C, para usar exit()
+#include <unistd.h>		// Unix standard, para usar close()
+#include <arpa/inet.h>	        // Estructuras para sockets
+#include <string.h>		// Para usar strlen() y strcmp()
+#include <netdb.h>		// Para usar gethostbyname()
+#include "defs.h"		// Mis propias definiciones
+
+int main (int argc, char ** argv) {
+	
+    int result, size;
+    int socketfd, clientfd;
+    socklen_t client_length;
+    struct sockaddr_in server_addr, client_addr;
+
+// Buffer para mandar y recibir datos
+    char buffer[BUFFER_SIZE];
+    char fnrecv[90];
+    char filename[100];
+    FILE * dest;
+
+// verifica los argumentos de la linea de comandos
+    if(argc !=2){
+        printf("invocar %s <port_donde_servir>\n", argv[0]);
+        return -1;
+    }
+
+// Abre un socket
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(!socketfd) {
+        perror("Error al abrir socket");
+        exit(ERROR);
+    }
+
+// Preparo la estructura de direccionamiento del server
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(atoi(argv[1]));
+    memset((void *) &(server_addr.sin_zero), '\0', 8); // Poner a cero el resto de la estructura
+
+// Vincula el socket con el puerto
+    result = bind(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if(result<0) {
+        perror("Error al bindear socket");
+        exit(ERROR);
+    }
+
+// Se pone a esperar conexiones que lleguen a ese puerto
+    result = listen(socketfd, BACKLOG);
+    if(result<0) {
+        perror("Error intentando escuchar en puerto");
+        exit(ERROR);
+    }
+
+    printf("Servidor conectado en puerto %d\n",atoi(argv[1]));
+	
+// Y cuando llega una conexion...
+    client_length = sizeof(client_addr);
+    while(1) { 
+        clientfd = accept(socketfd, (struct sockaddr *) &client_addr, &client_length);
+        if(clientfd<0) {
+            perror("Error recibiendo conexion");
+            exit(ERROR);
+        }
+			
+// recibe el nombre del archivo
+        read(clientfd, fnrecv, 90);
+
+// Construye un nombre de archivo y lo abre
+        sprintf(filename, "sent/%s", fnrecv);
+        dest = fopen(filename, "r");
+        printf("Solicitando archivo '%s'\n", filename);
+        if(dest == NULL) {
+            write(clientfd, "ER", 3);
+            printf("El archivo pedido %s es invalido\n", filename);
+        }
+        else {
+            write(clientfd, "OK", 3);
+            size = 0;
+            while((result = fread(buffer, 1, BUFFER_SIZE, dest))) {
+                size += result;
+                if(result>0) {
+                    write(clientfd, buffer, result);
+                }
+            }
+            printf("Enviados %d bytes de '%s' hacia el socket\n", size, filename);
+            fclose(dest);
+        }
+// Cierra socket al terminar
+        close(clientfd);
+    }
+    close(socketfd);
+    return SUCCESS;
+}
+
+
